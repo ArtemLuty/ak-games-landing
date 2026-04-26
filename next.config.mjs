90801d58353b29a2525f10b95249e1cc84ece1ec
Artem_K. <>
2026-04-25 23:08:41 -0700

    Add GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS === "true";
+const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "ak-games-landing";
+
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: isGithubActions ? `/${repoName}` : "",
+  assetPrefix: isGithubActions ? `/${repoName}/` : "",
 };
 
 export default nextConfig;
